db96c51d3112281f19e83618050a2c3b24d61b96
KavEn06 <kavinnimal@gmail.com>
2025-12-31 15:31:41 -0500

    resampled (refactored? ion know) the data to be based on distance not time)

--- src/processing/distance.py
+++ src/processing/distance.py
@@ -19,7 +19,8 @@ import constants
     df['Distance'] = distance
     df.to_csv(csv_path, index=False) """
 
-def calculate_distance(csv_path):
+def calculate_distance(lap_name):
+    csv_path = "data/raw/session_" + constants.SESSION_ID_PREFIX + "/" + lap_name
     df = pd.read_csv(csv_path)
 
     positions_columns = ['PostionX', 'PositionY', 'PositionZ']
@@ -42,8 +43,25 @@ def calculate_distance(csv_path):
 
     df.to_csv(csv_path, index=False)
 
-def resample(csv_path, interval=0.001):
+def resample(lap_name, interval=0.001):
+    csv_path = "data/raw/session_" + constants.SESSION_ID_PREFIX + "/" + lap_name
     df = pd.read_csv(csv_path)
+
+    processed_filepath = "data/processed/session_" + constants.SESSION_ID_PREFIX + "/" + lap_name
+    resampled_df = pd.DataFrame()
+
+    #distancenorm is intervals and actual distance will be laplength times distancenorm
+    resample_signals = ['CurrentEngineRpm', 'Speed', 'Power', 'Torque', 'Boost', 'Accel', 'Brake', 'Steer']
+
+    intervals = np.linspace(0, 1, int(1/interval))
+
+    resampled_df['DistanceNorm'] = intervals
+    resampled_df['Distance'] = intervals * df['Distance'].iloc[-1]
+
+    for signal in resample_signals:
+        resampled_df[signal] = np.interp(intervals, df['DistanceNorm'], df[signal])
+    
+    resampled_df.to_csv(processed_filepath, index=False)
 
 
 
